00fad7f5f34f3efe4d88abb0295bc639fc7b8d80
golzi99 <golzi99@gmail.com>
2024-08-27 11:32:32 +0300

    27.08.2024 06 - callbacks

---
+++ src/06-callbacks/06-callbacks.tsx
@@ -0,0 +1,35 @@
+// const callback = () => {
+//     alert('hey')
+// }
+//
+// window.setTimeout(callback, 1000)
+
+import App from '../App';
+import {ChangeEvent, MouseEvent} from 'react';
+
+export const User = () => {
+
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.name)
+    }
+
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
+
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.currentTarget.value)
+    }
+
+    const focusLostHandler = () => {
+        console.log('focus lost')
+    }
+
+    return (
+        <div>
+            <textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+            <input onChange={onAgeChanged} type={'number'}/>
+            <button name={'delete'} onClick={deleteUser}>x</button>
+        </div>
+    )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React from 'react';
 import './App.css';
+import {User} from './06-callbacks/06-callbacks';
 
 function App() {
-    // function getCount(str: string): number {
-    //     const vowels = ['a', 'e', 'i', 'o', 'u'];
-    //     let res: number = 0;
-    //     for (let i = 0; i < str.length; i++) {
-    //         if (vowels.includes(str[i])) {
-    //             res++;
-    //         }
-    //     }
-    //     return res
+    // function getCount(str: string) {
+    //     let list = str.match(/[aeiou]/gi);
+    //     return list ? list.length : 0;
     // }
     //
-    // console.log(getCount("abracadabra"))
-
-    function getCount(str: string) {
-        let list = str.match(/[aeiou]/gi);
-        return list ? list.length : 0;
-    }
-
-    console.log(getCount('abracadabra'))
+    // console.log(getCount('abracadabra'))
 
     return (
         <div>
-            <div>
-                <button></button>
-                <div></div>
-            </div>
+            <User></User>
         </div>
     );
 }
